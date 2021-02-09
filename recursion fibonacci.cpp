7f70eccaef898dc96ce646a96a57a856864a8e05
Mohammad Miraj Ansari <mirajhad@users.noreply.github.com>
2021-02-09 17:01:13 +0530

    Add files via upload

---
+++ recursion fibonacci.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int n)
+{
+    if(n==0)
+    return 0;
+    
+    if(n==1)
+    return 1;
+    
+    int smalloutput1 = fibonacci(n-1);
+    int smalloutput2 = fibonacci(n-2);
+    return smalloutput1+ smalloutput2;
+}
+int main() {
+	cout<<fibonacci(3)<<endl;
+	return 0;
+}
